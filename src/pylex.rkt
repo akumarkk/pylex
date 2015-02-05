aea5d5e25804b574cb55e0c6b1a0b57afee8da34
akumarkk <akumarkk@outlook.com>
2015-02-04 22:41:05 -0700

    few abbre

--- src/pylex.rkt
+++ src/pylex.rkt
@@ -4,10 +4,32 @@
 
 (define-lex-abbrev keyword (or "true" "false"))
 
+
+(define-lex-abbrev hash-comment ("#"))
+
+(define-lex-abbrev operator (or "+"      "-"     "*"     "**"     "/"      "//"    "%" 
+                                "<<"     ">>"    "&"     "^"      "|"      "^"     "~"  
+                                "<"      ">"     "<="    ">="     "=="     "!="))
+
+(define-lex-abbrev delimiter (or "("     ")"     "["     "]"      "{"      "}" 
+                                 ","     ":"     "."     ";"      "@"      "="     "->" 
+                                 "+="    "-="    "*="    "/="     "//="    "%="
+                                 "&="    "|="    "^="    ">>="    "<<="    "**="))
+
+
+
 (define basic-printing-lexer
   (lexer 
    
-   [(complement keyword)
+   [(: operator)
+    ; =>
+    (begin (display "found an operator  :  ")
+           (display lexeme)
+           (newline)
+           (basic-printing-lexer input-port))]
+   
+   
+     [(complement keyword)
     ; =>
     (begin (display "found a match :  ")
            (display lexeme)
@@ -36,6 +58,6 @@
 (run-basic-printing-lexer (open-input-string "foo"))
 (run-basic-printing-lexer (open-input-string "zoo"))
 
-(define in (open-input-string "true"))
+(define in (open-input-string "+"))
 (basic-printing-lexer in)
 
